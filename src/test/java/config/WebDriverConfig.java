f1e63c96465fe1d697821ce33337e1a6b462a0c5
alena.porohova <alena.porohova@lamoda.ru>
2023-07-05 00:46:13 +0300

    change WebDriverConfig

--- src/test/java/config/WebDriverConfig.java
+++ src/test/java/config/WebDriverConfig.java
@@ -7,14 +7,12 @@ import org.aeonbits.owner.Config;
 })
 public interface WebDriverConfig extends Config {
     @Key("browserName")
-    @DefaultValue("chrome")
+    @DefaultValue("CHROME")
     String getBrowserName();
     @Key("browserVersion")
-    @DefaultValue("100.0")
     String getBrowserVersion();
 
     @Key("browserSize")
-    @DefaultValue("1920x1080")
     String getBrowserSize();
 
     @Key("baseUrl")
